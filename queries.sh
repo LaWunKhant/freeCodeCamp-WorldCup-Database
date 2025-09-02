aceeb5de63bca70f8dfc0bd1a28d574873f04508
LaWunKhant <lawunkhant.mr3@gmail.com>
2025-09-01 23:09:55 -0400

    have changed the queries.sh

--- queries.sh
+++ queries.sh
@@ -6,7 +6,7 @@ PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c
 
 echo "$($PSQL "SELECT SUM(winner_goals) FROM games;")"
 
-echo "$($PSQL "SELECT SUM(winner_goals) + SUM(opponent_goals) FROM games")"
+echo "$($PSQL "SELECT SUM(winner_goals) + SUM(opponent_goals) FROM games;")"
 
 echo "$($PSQL "SELECT AVG(winner_goals) FROM games;")"
 
@@ -18,22 +18,12 @@ echo "$($PSQL "SELECT MAX(winner_goals) FROM games;")"
 
 echo "$($PSQL "SELECT COUNT(*) FROM games WHERE winner_goals > 2;")"
 
-echo "$($PSQL "SELECT COUNT(DISTINCT winner_id) FROM games;")"
+echo "$($PSQL "SELECT name FROM teams WHERE team_id = (SELECT winner_id FROM games WHERE year = 2018 AND round = 'Final');")"
 
-echo "$($PSQL "SELECT name FROM teams WHERE team_id=(SELECT winner_id FROM games WHERE round='Final' AND year=2018);")"
+echo "$($PSQL "SELECT name FROM teams WHERE team_id IN (SELECT winner_id FROM games WHERE year = 2014 AND round = 'Eighth-Final') OR team_id IN (SELECT opponent_id FROM games WHERE year = 2014 AND round = 'Eighth-Final') ORDER BY name;")"
 
-echo "$($PSQL "SELECT name FROM teams WHERE team_id IN (SELECT opponent_id FROM games WHERE round='Final' AND year=2014);")"
+echo "$($PSQL "SELECT DISTINCT(name) FROM teams LEFT JOIN games ON teams.team_id=games.winner_id WHERE games.winner_id IS NOT NULL ORDER BY name;")"
 
-echo "$($PSQL "SELECT name FROM teams WHERE team_id IN (SELECT winner_id FROM games WHERE year=2014) AND team_id IN (SELECT winner_id FROM games WHERE year=2018);")"
+echo "$($PSQL "SELECT T1.year, T2.name FROM games AS T1 JOIN teams AS T2 ON T1.winner_id=T2.team_id WHERE T1.round='Final' ORDER BY T1.year;")"
 
-echo "$($PSQL "SELECT name FROM teams WHERE team_id IN (SELECT team_id FROM teams FULL JOIN games ON teams.team_id = games.winner_id OR teams.team_id = games.opponent_id WHERE year=2014);")"
-
-echo "$($PSQL "SELECT DISTINCT(name) FROM teams LEFT JOIN games ON teams.team_id=games.winner_id WHERE round='Eighth-Final';")"
-
-echo "$($PSQL "SELECT round, COUNT(*) FROM games GROUP BY round ORDER BY COUNT(*) DESC LIMIT 1;")"
-
-echo "$($PSQL "SELECT year, round, winner_goals, opponent_goals, name AS winner FROM games JOIN teams ON games.winner_id = teams.team_id WHERE round='Final';")"
-
-echo "$($PSQL "SELECT DISTINCT(name) FROM teams LEFT JOIN games ON teams.team_id = games.winner_id WHERE name LIKE 'Co%';")"
-
-echo "$($PSQL "SELECT DISTINCT(name) FROM teams WHERE name LIKE '%ia';")"
+echo "$($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%';")"
